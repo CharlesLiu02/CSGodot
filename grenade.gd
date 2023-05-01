6b9023d8935d7eef6b00319ec2c41cd81f7ab7fd
Andrew Shen <shenandrew95@gmail.com>
2023-05-01 16:35:18 -0700

    Saving progress on volumetric smoke

--- grenade.gd
+++ grenade.gd
@@ -1,12 +1,12 @@
 extends RigidBody3D
 
 @export var smoke_volume = preload("res://smoke/smoke_volume.tscn")
+@export var square_smoke_volume = preload("res://smoke/square_smoke_volume.tscn")
 
 
 
 func _on_explode_timer_timeout():
-	var smoke = smoke_volume.instantiate()
+	var smoke = square_smoke_volume.instantiate()
 	get_parent().add_child(smoke)
-	print(get_parent().get_children())
 	smoke.global_position = global_position
 	queue_free()
