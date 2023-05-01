e1b473db83bb633757d66f99e29033b204f98de7
Andrew Shen <shenandrew95@gmail.com>
2023-05-01 15:00:39 -0700

    Working simple smoke grenades in multiplayer

--- grenade.gd
+++ grenade.gd
@@ -1,11 +1,12 @@
 extends RigidBody3D
 
-@export var smoke_volume: PackedScene
+@export var smoke_volume = preload("res://smoke/smoke_volume.tscn")
 
 
 
 func _on_explode_timer_timeout():
 	var smoke = smoke_volume.instantiate()
 	get_parent().add_child(smoke)
+	print(get_parent().get_children())
 	smoke.global_position = global_position
 	queue_free()
